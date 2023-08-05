930b68542e27a7b3100814624962cfb446b057cf
John Ernelle Alba <johnernellealbaw@gmail.com>
2023-08-06 00:49:46 +0800

    :construction:Added admin panel

--- src/pages/admin/AdminPanel.jsx
+++ src/pages/admin/AdminPanel.jsx
@@ -1,5 +1,8 @@
 import { useEffect } from "react"
 import "./AdminPanel.css"
+import avatar from "../../images/avatar.png"
+import logo from "../../images/Concert-Craft-Logo-Transparent.png"
+
 
 function AdminPanel() {
 
@@ -10,7 +13,17 @@ function AdminPanel() {
   return (
     <>
       <div className="admin-panel-container">
-        <h1>Admin Panel</h1>
+        <div className="admin-panel-header">
+          <div className="logo-container">
+            <img src={logo} alt="concert-craft-logo" />
+            <h4>Admin Dashboard</h4>
+          </div>
+          <div className="avatar-container">
+            <h4>Welcome Admin</h4>
+            <img src={avatar} alt="admin-avatar"/>
+          </div>
+          
+        </div>
       </div>
     </>
   )
