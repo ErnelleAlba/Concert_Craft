1921e1e41b35d2d4180f19aabfd311c04d80141f
John Ernelle Alba <johnernellealbaw@gmail.com>
2023-08-10 00:24:29 +0800

    :construction: Working on users table in admin dashboard

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.esm.js'
+import 'font-awesome/css/font-awesome.min.css'
+import 'font-awesome/fonts/fontawesome-webfont.svg'
+import 'datatables.net-bs5/css/dataTables.bootstrap5.min.css'
+import 'datatables.net-buttons-bs5/css/buttons.bootstrap5.min.css'
+import 'datatables.net-buttons-bs5/js/buttons.bootstrap5.min.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  // <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  /* </React.StrictMode>, */
 )
