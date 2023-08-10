f5fe55c79ab1278988492e481d35652c07c87b58
John Ernelle Alba <johnernellealbaw@gmail.com>
2023-08-11 01:33:43 +0800

    :construction: Added Dummy Users Table

--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,13 @@ import 'font-awesome/fonts/fontawesome-webfont.svg'
 import 'datatables.net-bs5/css/dataTables.bootstrap5.min.css'
 import 'datatables.net-buttons-bs5/css/buttons.bootstrap5.min.css'
 import 'datatables.net-buttons-bs5/js/buttons.bootstrap5.min.js'
+import 'datatables.net-searchpanes-bs5/css/searchPanes.bootstrap5.min.css'
+import 'datatables.net-searchpanes-bs5/js/searchPanes.bootstrap5.min.js'
+import 'datatables.net-fixedheader-bs5/js/fixedHeader.bootstrap5.min.js'
+import 'datatables.net-fixedheader-bs5/css/fixedHeader.bootstrap5.min.css'
+import 'datatables.net-responsive-bs5/css/responsive.bootstrap5.min.css'
+import 'datatables.net-responsive-bs5/js/responsive.bootstrap5.min.js'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
