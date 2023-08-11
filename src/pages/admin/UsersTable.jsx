d31db27945bb1b9aafa0f6cdfd7bdbf4d88e6a2c
John Ernelle Alba <johnernellealbaw@gmail.com>
2023-08-11 09:23:38 +0800

    Fixing import error(2)

--- src/pages/admin/UsersTable.jsx
+++ src/pages/admin/UsersTable.jsx
@@ -1,11 +1,10 @@
 import DataTable from "datatables.net-autofill-bs5"
 import jszip from 'jszip'
 DataTable.Buttons.jszip( jszip );
-import 'pdfmake/build/pdfmake.min.js'
-import 'pdfmake/build/vfs_fonts.js'
+
 import pdfMake from 'pdfmake/build/pdfmake.min.js'
-import pdfFonts from 'pdfmake/build/vfs_fonts.js'
-pdfMake.vfs = pdfFonts.pdfMake.vfs
+import 'pdfmake/build/vfs_fonts.js'
+// pdfMake.vfs = pdfFonts.pdfMake.vfs
 import 'datatables.net-responsive-bs5'
 import 'datatables.net-fixedheader-bs5'
 // import 'datatables.net-scroller-bs5'
