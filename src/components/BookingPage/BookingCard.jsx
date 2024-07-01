89bf8e7649fb0b637a03c0603024a1f2f8b500cb
John Ernelle Alba <johnernellealbaw@gmail.com>
2024-07-01 16:43:16 +0800

    :bug: Fix poster image is broken when searching

--- src/components/BookingPage/BookingCard.jsx
+++ src/components/BookingPage/BookingCard.jsx
@@ -1,10 +1,9 @@
-import axios from 'axios';
-import { Alert, Toast } from 'bootstrap';
-import { useFormik } from 'formik';
-import React, { useEffect, useState } from 'react';
-import { useSelector } from 'react-redux';
-import * as Yup from 'yup'
-
+import axios from "axios";
+import { Alert, Toast } from "bootstrap";
+import { useFormik } from "formik";
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import * as Yup from "yup";
 
 const seatOptions = [
   { value: "vip_seat", name: "VIP Seat" },
@@ -12,31 +11,35 @@ const seatOptions = [
   { value: "regular_seat", name: "Regular Seat" },
 ];
 
-function BookingCard ({id}) { //ConcertCard props
+function BookingCard({ id }) {
+  //ConcertCard props
 
-  const token = useSelector(state => state.token)
+  const token = useSelector((state) => state.token);
 
   const totalAmount = () => {
-    return formik.values.ticketItem * concert.ticketPrice
-  }
-
+    return formik.values.ticketItem * concert.ticketPrice;
+  };
 
   const formik = useFormik({
     initialValues: {
-      ticketItem: '',
+      ticketItem: "",
       seatValue: seatOptions[0].value,
     },
 
     validationSchema: Yup.object({
-      ticketItem: Yup.number().integer("Must be a number").positive("Must be positive number").required("Input number of tickets"),
+      ticketItem: Yup.number()
+        .integer("Must be a number")
+        .positive("Must be positive number")
+        .required("Input number of tickets"),
       seatValue: Yup.string().required("Seat Position is required"),
     }),
 
     onSubmit: async (value) => {
-      console.log(value)
+      console.log(value);
 
       try {
-        const res = await axios.post('http://localhost:8000/api/v1/bookings',
+        const res = await axios.post(
+          "http://localhost:8000/api/v1/bookings",
           {
             concertId: id,
             seatPosition: value.seatValue,
@@ -45,60 +48,67 @@ function BookingCard ({id}) { //ConcertCard props
           },
           {
             headers: {
-              'Content-Type': 'application/x-www-form-urlencoded',
-              'Authorization': `Bearer ${token}`
-            }
+              "Content-Type": "application/x-www-form-urlencoded",
+              Authorization: `Bearer ${token}`,
+            },
           }
-        )
-        console.log(res)
+        );
+        console.log(res);
         // console.log(value.seatValue)
         if (res.status === 201) {
-          alert('Your booking is successful check the email we send to you for details. Thank you for booking!!')
+          alert(
+            "Your booking is successful check the email we send to you for details. Thank you for booking!!"
+          );
         }
       } catch (err) {
-        alert('Your booking failed. Try again.')
+        alert("Your booking failed. Try again.");
       }
-      
-
-    }
-  })
+    },
+  });
 
   const [concert, setConcert] = useState({
-    title: '',
-    posterImageUrl: '',
-    description: '',
-    eventDate: '',
-    eventPlace: '',
-    ticketPrice: '',
-  })
+    title: "",
+    posterImageUrl: "",
+    description: "",
+    eventDate: "",
+    eventPlace: "",
+    ticketPrice: "",
+  });
 
   const fetchConcertInfo = async () => {
-    const res = await axios(`http://localhost:8000/api/v1/concerts/${id}`)
-    const concertInfo = res.data.data
+    const res = await axios(`http://localhost:8000/api/v1/concerts/${id}`);
+    const concertInfo = res.data.data;
     // console.log(concertInfo)
     const concertObj = {
       title: concertInfo.title,
-      posterImageUrl: concertInfo.posterImageUrl.replace('storage','http://localhost:8000/storage'),
+      posterImageUrl: concertInfo.posterImageUrl.replace(
+        "storage",
+        "http://localhost:8000/storage"
+      ),
       description: concertInfo.description,
       eventDate: concertInfo.eventDate,
       eventPlace: concertInfo.eventPlace,
       ticketPrice: concertInfo.ticketPrice,
-    }
+    };
 
-    setConcert(concertObj)
-  }
+    setConcert(concertObj);
+  };
 
   useEffect(() => {
     fetchConcertInfo();
     totalAmount();
-  }, [])
-  
+  }, []);
+
   return (
     <>
       <div className="card w-100 shadow-lg">
         <div className="row no-gutters">
           <div className="col-md-4">
-            <img src={concert.posterImageUrl} className="img-fluid rounded border border-black w-100" alt={concert.title} />
+            <img
+              src={concert.posterImageUrl}
+              className="img-fluid rounded border border-black w-100"
+              alt={concert.title}
+            />
           </div>
           <div className="col-md-8">
             <div className="card-body">
@@ -106,78 +116,109 @@ function BookingCard ({id}) { //ConcertCard props
               <p className="card-text">{concert.description}</p>
               <div className="card-text d-flex flex-column gap-1 mb-3">
                 <small className="text-muted">
-                <i className="fa-solid fa-location-dot me-1"></i>
-                  Place:<span className="ms-1">
-                    {concert.eventPlace}</span></small>
+                  <i className="fa-solid fa-location-dot me-1"></i>
+                  Place:<span className="ms-1">{concert.eventPlace}</span>
+                </small>
                 <small className="text-muted">
-                <i className="fa-regular fa-calendar me-1"></i>
-                  Date: <span className="ms-1">
-                    {concert.eventDate.split("-")[0]}</span></small>
+                  <i className="fa-regular fa-calendar me-1"></i>
+                  Date:{" "}
+                  <span className="ms-1">
+                    {concert.eventDate.split("-")[0]}
+                  </span>
+                </small>
                 <small className="text-muted">
-                <i className="fa-regular fa-clock me-1"></i>
-                  Time: <span className="ms-1">
-                    {concert.eventDate.split("-")[1]}</span></small>
+                  <i className="fa-regular fa-clock me-1"></i>
+                  Time:{" "}
+                  <span className="ms-1">
+                    {concert.eventDate.split("-")[1]}
+                  </span>
+                </small>
                 <h5 className="card-text text-danger mt-2">
-                  <span className='text-dark me-2'>
-                    Ticket Price: </span><i className="fa-solid fa-peso-sign me-1"></i>
-                    {concert.ticketPrice}</h5>
+                  <span className="text-dark me-2">Ticket Price: </span>
+                  <i className="fa-solid fa-peso-sign me-1"></i>
+                  {concert.ticketPrice}
+                </h5>
               </div>
-              <form onSubmit={formik.handleSubmit} autoComplete='off' noValidate>
+              <form
+                onSubmit={formik.handleSubmit}
+                autoComplete="off"
+                noValidate
+              >
                 <div className="d-flex mb-2 col-md-6 column">
-                  <label htmlFor="ticketItem" className='form-label me-3'>Number of Tickets:</label>
-                  <div className='row col'>
-                  <input 
-                    type="number"
-                    name='ticketItem' 
-                    className={
-                        formik.errors.ticketItem && formik.touched.ticketItem 
-                          ? 
-                          "is-invalid form-control" 
-                          : 
-                          "form-control"
-                      }  
-                    id={`ticketItem-${id}`}
-                    value={formik.values.ticketItem}
-                    onChange={formik.handleChange}
-                    onBlur={formik.handleBlur}
+                  <label htmlFor="ticketItem" className="form-label me-3">
+                    Number of Tickets:
+                  </label>
+                  <div className="row col">
+                    <input
+                      type="number"
+                      name="ticketItem"
+                      className={
+                        formik.errors.ticketItem && formik.touched.ticketItem
+                          ? "is-invalid form-control"
+                          : "form-control"
+                      }
+                      id={`ticketItem-${id}`}
+                      value={formik.values.ticketItem}
+                      onChange={formik.handleChange}
+                      onBlur={formik.handleBlur}
                     />
-                    {
-                      formik.errors.ticketItem && formik.touched.ticketItem 
-                      ? 
-                      <span className="text-danger">{formik.errors.ticketItem}</span> 
-                      : 
-                      null
-                    }
+                    {formik.errors.ticketItem && formik.touched.ticketItem ? (
+                      <span className="text-danger">
+                        {formik.errors.ticketItem}
+                      </span>
+                    ) : null}
                   </div>
                 </div>
-                <div className='mb-2'>
-                  <label htmlFor="seatPosition" className='me-2'>Select an option:</label>
+                <div className="mb-2">
+                  <label htmlFor="seatPosition" className="me-2">
+                    Select an option:
+                  </label>
                   <select
                     name="seatPosition"
                     value={formik.values.seatValue}
                     onBlur={formik.handleBlur}
                     onChange={(e) => {
-                      formik.setFieldValue("seatValue", e.target.value)
+                      formik.setFieldValue("seatValue", e.target.value);
                     }}
                   >
-                    {seatOptions.map((option) => 
+                    {seatOptions.map((option) => (
                       <option key={option.value} value={option.value}>
                         {option.name}
                       </option>
-                    )}
+                    ))}
                   </select>
                 </div>
                 <p>You selected: {formik.values.seatValue}</p>
-                <h4 className="pb-1">Total Amount: <i className="fa-solid fa-peso-sign text-danger me-1"></i><span className='text-danger'>{totalAmount()}</span></h4>
-                <button type="submit" className="btn btn-dark btn-lg btn-block col-md-12 text-light mt-2"><i className="fa-solid fa-ticket me-2"></i> Book your tickets</button>
+                <h4 className="pb-1">
+                  Total Amount:{" "}
+                  <i className="fa-solid fa-peso-sign text-danger me-1"></i>
+                  <span className="text-danger">{totalAmount()}</span>
+                </h4>
+                <button
+                  type="submit"
+                  className="btn btn-dark btn-lg btn-block col-md-12 text-light mt-2"
+                >
+                  <i className="fa-solid fa-ticket me-2"></i> Book your tickets
+                </button>
               </form>
               <div className="toast-container position-fixed top-0 end-0 p-3 mt-5 m">
-                <div id="bookingToast" className="toast align-items-center text-bg-success border-0" role="alert" aria-live="assertive" aria-atomic="true">
+                <div
+                  id="bookingToast"
+                  className="toast align-items-center text-bg-success border-0"
+                  role="alert"
+                  aria-live="assertive"
+                  aria-atomic="true"
+                >
                   <div className="d-flex">
                     <div className="toast-body">
                       Tickets successfully booked !
                     </div>
-                    <button type="button" className="btn-close btn-close-white me-2 m-auto" data-bs-dismiss="toast" aria-label="Close"></button>
+                    <button
+                      type="button"
+                      className="btn-close btn-close-white me-2 m-auto"
+                      data-bs-dismiss="toast"
+                      aria-label="Close"
+                    ></button>
                   </div>
                 </div>
               </div>
@@ -186,9 +227,8 @@ function BookingCard ({id}) { //ConcertCard props
           </div>
         </div>
       </div>
-
     </>
-  )
+  );
 }
 
-export default BookingCard
+export default BookingCard;
--- src/pages/ScheduleBooking.jsx
+++ src/pages/ScheduleBooking.jsx
@@ -1,84 +1,90 @@
-import { useEffect } from "react"
-import Footer from "../components/Footer"
-import NavBar from "../components/NavBar"
-import FilterPanel from "../components/SchedulePage/FilterPanel"
-import "./ScheduleBooking.css"
-import { useDispatch, useSelector } from "react-redux"
-import LoadingIcon from "../components/LoadingIcon"
-import ConcertScheduleCard from "../components/SchedulePage/ConcertScheduleCard"
-import { markLoading, unmarkLoading } from "../store/isLoadingReducers"
-import { setConcerts } from "../store/concertsReducers"
-import axios from "axios"
-
+import { useEffect } from "react";
+import Footer from "../components/Footer";
+import NavBar from "../components/NavBar";
+import FilterPanel from "../components/SchedulePage/FilterPanel";
+import "./ScheduleBooking.css";
+import { useDispatch, useSelector } from "react-redux";
+import LoadingIcon from "../components/LoadingIcon";
+import ConcertScheduleCard from "../components/SchedulePage/ConcertScheduleCard";
+import { markLoading, unmarkLoading } from "../store/isLoadingReducers";
+import { setConcerts } from "../store/concertsReducers";
+import axios from "axios";
 
 function ScheduleBooking() {
-  const concertCard = useSelector(state=>state.concerts)
-  const isLoading = useSelector(state => state.isLoading)
+  const concertCard = useSelector((state) => state.concerts);
+  const isLoading = useSelector((state) => state.isLoading);
   const dispatch = useDispatch();
 
   const handleOnChange = async (e) => {
-    dispatch(markLoading())
+    dispatch(markLoading());
 
-    const resTitle = await axios(`http://localhost:8000/api/v1/concerts?title=${e.target.value}`)
-    
-      dispatch(setConcerts(resTitle.data.data.map(
-      concert => {
-        return {
-          id: concert.id,
-          title: concert.title,
-          posterImageUrl: concert.posterImageUrl,
-          description: concert.description,
-          eventPlace: concert.eventPlace,
-          eventDate: concert.eventDate,
-          ticketPrice: concert.ticketPrice,
-        }
-      }
-    )))
+    const resTitle = await axios(
+      `http://localhost:8000/api/v1/concerts?title=${e.target.value}`
+    );
 
-    dispatch(unmarkLoading())
-    // console.log(e.target.value)
-  }
+    dispatch(
+      setConcerts(
+        resTitle.data.data.map((concert) => {
+          return {
+            id: concert.id,
+            title: concert.title,
+            posterImageUrl: concert.posterImageUrl.replace(
+              "storage",
+              "http://localhost:8000/storage"
+            ),
+            description: concert.description,
+            eventPlace: concert.eventPlace,
+            eventDate: concert.eventDate,
+            ticketPrice: concert.ticketPrice,
+          };
+        })
+      )
+    );
+
+    dispatch(unmarkLoading());
+    console.log(resTitle.data.data);
+  };
 
   useEffect(() => {
-    document.title = "Schedules | Concert Craft"
-  }, [])
+    document.title = "Schedules | Concert Craft";
+  }, []);
 
   return (
     <>
       <NavBar />
-        <div className="schedule-title-container" >
-          <h1>Scheduled Concerts</h1>
-          <h3>Upcoming Concerts</h3>
+      <div className="schedule-title-container">
+        <h1>Scheduled Concerts</h1>
+        <h3>Upcoming Concerts</h3>
+      </div>
+      <br />
+      <div className="d-flex flex-column">
+        <div className="d-flex justify-content-center align-self-end col-10 col-lg-2 col-md-4 col-sm-5 me-5 mb-2 bg-dark text-light px-2 py-1 rounded">
+          <label htmlFor="search-concert-bar" className="me-2 pt-2 form-label">
+            Search:{" "}
+          </label>
+          <input
+            type="text"
+            id="search-concert-bar"
+            className="form-control"
+            onChange={handleOnChange}
+          />
         </div>
-        <br />
-        <div className="d-flex flex-column">
-          <div className="d-flex justify-content-center align-self-end col-10 col-lg-2 col-md-4 col-sm-5 me-5 mb-2 bg-dark text-light px-2 py-1 rounded">
-            <label htmlFor="search-concert-bar" className="me-2 pt-2 form-label">Search: </label>
-            <input type="text"  id="search-concert-bar" className="form-control" onChange={handleOnChange} />
-          </div>
-          <FilterPanel/>
-          <div className="container-fluid mb-5 d-flex flex-wrap justify-content-center">
-              {
-                isLoading 
-                  ? 
-                <LoadingIcon /> 
-                  : 
-                  (
-                    concertCard.length 
-                      ? 
-                    concertCard.map((concertCard, index)=> <ConcertScheduleCard 
-                      key={index} 
-                      concertCard={concertCard}/>) 
-                      : 
-                    <h1 className="my-5 py-5">No Concert Found</h1>  
-                  )
-                
-              }
-          </div>
+        <FilterPanel />
+        <div className="container-fluid mb-5 d-flex flex-wrap justify-content-center">
+          {isLoading ? (
+            <LoadingIcon />
+          ) : concertCard.length ? (
+            concertCard.map((concertCard, index) => (
+              <ConcertScheduleCard key={index} concertCard={concertCard} />
+            ))
+          ) : (
+            <h1 className="my-5 py-5">No Concert Found</h1>
+          )}
         </div>
-      <Footer/>
+      </div>
+      <Footer />
     </>
-  )
+  );
 }
 
-export default ScheduleBooking
+export default ScheduleBooking;
